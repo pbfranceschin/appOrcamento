7d326a1d18595db273c504e75d2b0960349b25a7
Spooner <pbfranceschin@gmail.com>
2023-01-09 22:09:09 -0300

    scan v1 working

--- packages/frontend/pages/scan.js
+++ packages/frontend/pages/scan.js
@@ -1,74 +1,78 @@
 import { parse } from "@ethersproject/transactions";
 import { ethers } from "ethers";
-import React from "react";
-import { useState } from "react";
+import React, { useEffect } from "react";
+import { useState, useRef } from "react";
 import { getContractData } from '../utils';
+import { useProvider } from 'wagmi';
 
 
 const fetchTxData = async (provider, contract, filter) => {
     
-    const logs = await provider.getLogs(filter);
+    // const logs = await provider.getLogs(filter);
+    let logs = await contract.queryFilter(filter)
 
-    const events = logs.map(log => contract.interface.parseLog(log));
+    console.log(logs)
 
-    return events.slice(-5);
+    const events = logs.map(log => contract.interface.parseLog(log).args);
+
+    return events.slice(-10);
 }
 
+const useTxData = () => {
+    const [txData, setTxData] = useState([]);
+    const filterInitializer = useRef(false);
+    const provider = useProvider();
+    useEffect(() => {
+        const [contractAddress, contractABI] = getContractData();
+        const contract = new ethers.Contract(contractAddress, contractABI, provider)
+        const filter = contract.filters.TransferSingle()
+        // console.log(provider)
+        if(!filterInitializer.current) {
+            provider.on(filter, (log) => {
+                // console.log(log)
+                const parsedEvent = contract.interface.parseLog({
+                    topics: log.topics,
+                    data: log.data,
+                })
+                // console.log(parsedEvent.args)
+                fetchTxData(provider, contract, filter).then(response => {
+                    setTxData(response)
+                })
+            })
+            filterInitializer.current = true
+        }
+    },[])
+    return txData
+}
 
-const main = async () => {
+const main = () => {
     
-    const [txData, setTxData] = useState('');
+    
 
-    const provider = new ethers.providers.JsonRpcProvider();
+    // const provider = new ethers.providers.JsonRpcProvider();
 
-    const [contractAddress, contractABI] = getContractData();
+    const txData = useTxData()
 
-    const contract = new ethers.Contract(contractAddress, contractABI, provider);
-
-    const filter = {
-        address: contractAddress,
-        topics: [
-          ethers.utils.id(
-            "TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value)"
-            )
-        ]
-    };
-
-    let events_ = await fetchTxData(provider, contract, filter)
-    setTxData(events_)
     console.log(txData)
-
-
-
-    // const transferSingleFilter = contract.filters.TransferSingle();
     
-    // let txOperators = new Array();
-    // let txSenders = new Array();
-    // let txReceivers = new Array();
-    // let txAreas = new Array();
-    // let txValues = new Array();
 
-    provider.on(filter, async (log) => {
-        // const parsedEvent = contract.interface.parseLog({
-        //     topics: log.topics,
-        //     data: log.data,
-        // })
-        // console.log('TransferSingle Event:')
-        // console.log('operator:', parsedEvent.args.operator)
-        // console.log('from', parsedEvent.args.from)
-        // console.log('to', parsedEvent.args.to )
-        // console.log('area', parsedEvent.args.id.toString())
-        // console.log('value', parsedEvent.args.value.toString())
-        // txOperators.push(parsedEvent.args.operator)
-        // txSenders.push(parsedEvent.args.from)
-        // txReceivers.push(parsedEvent.args.to)
-        // txAreas.push(parsedEvent.args.id)
-        // txValues.push(parsedEvent.args.value)
-        events_ = await fetchTxData(provider, contract, filter)
-        console.log(events_)
-    })
+    return (
+        <>
+        <main>
+            {txData.map((e,i) => {
+                const id = e.id.toString()
+                const value =e.value.toString()
+                return (
+                    <div key={id}>
+                    #{i}, {e.operator}, {e.from}, {e.to}, {id}, {value}
+                    </div>
+                )
 
-    return 
+            })}
+        </main>
+        
+        </>
+    )
 };
 
 export default main;
